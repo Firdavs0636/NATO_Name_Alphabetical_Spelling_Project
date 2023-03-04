0dbaf4ec57cc1701efd3f4d80b6a33e90cdf5634
Firdavs0636 <65146478+Firdavs0636@users.noreply.github.com>
2023-03-04 22:19:39 +0900

    new

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pandas as pd
 
-# TODO 1. Create a dictionary in this format:
 df = pd.read_csv('nato_phonetic_alphabet.csv')
 phonetic_dictionary = {row.letter: row.code for (index, row) in df.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
 encrypted = []
-user_name = input('Enter Your Name: ').upper()
-output_list = [phonetic_dictionary[letter] for letter in user_name]
-print(output_list)
 
+
+def generate_phonetic():
+    user_name = input('Enter Your Name: ').upper()
+    try:
+        output_list = [phonetic_dictionary[letter] for letter in user_name]
+    except KeyError:
+        print('Only ALPHABETICAL characters please!')
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
 
 
 # Below code is another approach for this problem which uses dictionary key:value as a letter:code combination

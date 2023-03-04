1c28dbba3f8d57cfafd4eb9012d663a0763651e5
Firdavs0636 <65146478+Firdavs0636@users.noreply.github.com>
2023-03-04 22:09:04 +0900

    new

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-list = [1, 2, 3, 4, 5]
-names = ['Firdavs', 'Abbos', 'Bekzod', 'Temurxon', 'Azizxon', 'Tima', 'Karl']
+import pandas as pd
 
-new_list = [name.upper() for name in names if len(name) > 5]
-print(new_list)
+# TODO 1. Create a dictionary in this format:
+df = pd.read_csv('nato_phonetic_alphabet.csv')
+phonetic_dictionary = {row.letter: row.code for (index, row) in df.iterrows()}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+encrypted = []
+user_name = input('Enter Your Name: ').upper()
+output_list = [phonetic_dictionary[letter] for letter in user_name]
+print(output_list)
+
+
+
+# Below code is another approach for this problem which uses dictionary key:value as a letter:code combination
+# for letter in user_name:
+#     name_letters = {letter: code for (key, code) in phonetic_dictionary.items() if letter == key}
+#     encrypted.append(name_letters)
+
